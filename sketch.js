5b5e2ffb2313f3dd5818634ed5aeae19a591b147
rntmdgus <rntmdgus@naver.com>
2014-11-18 19:24:44 +0900

    23432
    4243

--- sketch.js
+++ sketch.js
@@ -28,10 +28,10 @@ function draw() {
 	ellipse(1000,200,4000,1500)
 
  	//스시
-	image(earth,1000 + Math.cos(timing1*2*PI)*2250-150,1000 + Math.sin(timing1*2*PI)*500-150,300,300);
+	image(earth,1000 + Math.cos(timing1*2*PI)*2250,1000 + Math.sin(timing1*2*PI)*500,300,300);
 
 	//스시2
-	image(earth,1000 + Math.cos(timing2+5000*2*PI)*2250-150,1000 + Math.sin(timing2+5000*2*PI)*500-150,300,300);
+	image(earth,1000 + Math.cos(timing2*2*PI)*2250+5000,1000 + Math.sin(timing2*2*PI)*500+5000,300,300);
 
 	//고양이(태양)
  	image(solar,750,400,500,500);
