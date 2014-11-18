6a8fb028bfcb244cdd036dbff89c95f091ce1c4e
rntmdgus <rntmdgus@naver.com>
2014-11-18 19:59:40 +0900

    324233
    4234234

--- sketch.js
+++ sketch.js
@@ -29,10 +29,10 @@ function draw() {
 	//위라인
 	noFill();
 	stroke(255);
-	ellipse(1000,1300,4500,1500)
+	ellipse(1000,1300,4200,1500)
 
  	//위스시1-새우
-	image(sushi1,1000 + Math.cos(timing1*2*PI)*2250-150,1300 + Math.sin(timing1*2*PI)*750-95,300,190);
+	image(sushi1,1000 + Math.cos(timing1*2*PI)*2100-150,1300 + Math.sin(timing1*2*PI)*750-95,300,190);
 
 
 	//고양이(태양)
