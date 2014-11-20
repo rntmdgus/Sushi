d470eeccab3391ef28c0b06230d22c59f352ad73
rntmdgus <rntmdgus@naver.com>
2014-11-20 13:29:21 +0900

    434
    34

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var bg, chopsticks, left, right, count1, count2, cat1, cat2, sushi1, dishw;
+var bg, chopsticks, left, right, count1, count2, cat1, cat2, sushi1, sushi2, sushi3, dishw, dishy, dishr, dishb;
 
 function setup() {
 
@@ -7,13 +7,18 @@ function setup() {
     bg = loadImage("bg.png");
     chopsticks = loadImage("chopsticks.png");
     sushi1 = loadImage("sushi1.png");
+    sushi2 = loadImage("sushi2.png");
+    sushi3 = loadImage("sushi3.png");
     dishw = loadImage("dishw.png");
+    dishy = loadImage("dishy.png");
+    dishr = loadImage("dishr.png");
+    dishb = loadImage("dishb.png");
     left = loadImage("left.png");
     right = loadImage("right.png");
     cat1 = loadImage("cat1.png");
     cat2 = loadImage("cat2.png");
 
-	count1 = -2000;
+	count1 = 0;
 	count2 = 0;
 }
 
@@ -33,7 +38,7 @@ function draw() {
 	//발왼쪽 이동
 	image(left,count1,0);
 	count1=count1+30;
-	if(count1>0){
+	if(count1>-2000){
 	count1=0;
 	} 
 
@@ -49,9 +54,19 @@ function draw() {
 	stroke(255);
 	ellipse(1000,1300,4000,1500);
 
- 	//위스시1-새우접시
+ 	//위스시1-새우접시w
 	image(dishw,1000 + Math.cos(timing1*2*PI)*2000-250,1300 + Math.sin(timing1*2*PI)*750-100,500,200);
  	//위스시1-새우
+	image(sushi1,1000 + Math.cos(timing1*2*PI)*2000-136,1300 + Math.sin(timing1*2*PI)*750-99,272,158);
+
+ 	//위스시2-참치접시y
+	image(dishy,1000 + Math.cos(timing1*2*PI)*2000-250,1300 + Math.sin(timing1*2*PI)*750-100,500,200);
+ 	//위스시2-참치
+	image(sushi2,1000 + Math.cos(timing1*2*PI)*2000-136,1300 + Math.sin(timing1*2*PI)*750-99,272,158);
+
+	//위스시3-소고기접시r
+	image(dishw,1000 + Math.cos(timing1*2*PI)*2000-250,1300 + Math.sin(timing1*2*PI)*750-100,500,200);
+ 	//위스시3-소고기
 	image(sushi1,1000 + Math.cos(timing1*2*PI)*2000-136,1300 + Math.sin(timing1*2*PI)*750-99,272,158);
 
 
@@ -71,6 +86,10 @@ function draw() {
 	image(earth,1000 + Math.cos(timing1*2*PI)*2250-300,120 + Math.sin(timing1*2*PI)*750-300,600,600);
 
 	//젓가락
-	image(chopsticks,mouseX,mouseY,700,700);
+	draw1
 
+}
+
+function draw1(){
+	image(chopsticks,mouseX,mouseY,700,700);
 }
