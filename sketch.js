f625471ab905181828a725f425d2b63866a9e0b7
rntmdgus <rntmdgus@naver.com>
2014-11-20 15:07:28 +0900

    4234
    43

--- sketch.js
+++ sketch.js
@@ -51,12 +51,12 @@ function draw() {
 	if(50*sin(t*2*PI)<0){
 		image(sushi1,1000+cos(t*2*PI)*1050,580+150*sin(t*2*PI),272,158);
 	}
-	if(50*sin(t*2*PI+PI/3)<0){
-		image(sushi2,1000+cos(t*2*PI+PI/6)*1050,580+150*sin(t*2*PI+PI/3),272,158);
+	if(50*sin(t*2*PI+PI/6)<0){
+		image(sushi2,1000+cos(t*2*PI+PI/6)*1050,580+150*sin(t*2*PI+PI/6),272,158);
 		
 	}
 	if(50*sin(t*2*PI+PI/3)<0){
-		image(sushi2,1000+cos(t*2*PI+PI/4)*1050,580+150*sin(t*2*PI+PI/3),272,158);
+		image(sushi2,1000+cos(t*2*PI+PI/3)*1050,580+150*sin(t*2*PI+PI/3),272,158);
 		
 	}
 
@@ -77,12 +77,12 @@ function draw() {
 	if(50*sin(t*2*PI)>=0){
 		image(sushi1,1000+cos(t*2*PI)*1050,580+150*sin(t*2*PI),372,158);
 	}
-	if(50*sin(t*2*PI+PI/3)>=0){
-		image(sushi2,1000+cos(t*2*PI+PI/6)*1050,580+150*sin(t*2*PI+PI/3),372,158);
+	if(50*sin(t*2*PI+PI/6)>=0){
+		image(sushi2,1000+cos(t*2*PI+PI/6)*1050,580+150*sin(t*2*PI+PI/6),372,158);
 		
 	}
 	if(50*sin(t*2*PI+PI/3)>=0){
-		image(sushi2,1000+cos(t*2*PI+PI/4)*1050,580+150*sin(t*2*PI+PI/3),372,158);
+		image(sushi2,1000+cos(t*2*PI+PI/3)*1050,580+150*sin(t*2*PI+PI/3),372,158);
 		
 	}
 
