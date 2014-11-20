d63d87cfbaba52d42589bd634d27a80e62a09d82
rntmdgus <rntmdgus@naver.com>
2014-11-20 14:47:24 +0900

    dfgdf
    gfg

--- sketch.js
+++ sketch.js
@@ -53,10 +53,10 @@ function draw() {
 
 	if(50*sin(t*2*PI)<0){
 		//위
-		image(sushi1,1000+cos(t*2*PI)*1000,500+200*sin(t*2*PI),272,158);
+		image(sushi1,1000+cos(t*2*PI)*1000,600+150*sin(t*2*PI),272,158);
 	}
 	if(50*sin(t*2*PI+PI/3)<0){
-		image(sushi2,1000+cos(t*2*PI+PI/6)*1000,500+200*sin(t*2*PI+PI/3),272,158);
+		image(sushi2,1000+cos(t*2*PI+PI/6)*1000,600+150*sin(t*2*PI+PI/3),272,158);
 		
 	}
 
@@ -76,10 +76,10 @@ function draw() {
 
 	if(50*sin(t*2*PI)>=0){
 		//위
-		image(sushi1,1000+cos(t*2*PI)*1000,500+200*sin(t*2*PI),372,158);
+		image(sushi1,1000+cos(t*2*PI)*1000,600+150*sin(t*2*PI),372,158);
 	}
 	if(50*sin(t*2*PI+PI/3)>=0){
-		image(sushi2,1000+cos(t*2*PI+PI/6)*1000,500+200*sin(t*2*PI+PI/3),372,158);
+		image(sushi2,1000+cos(t*2*PI+PI/6)*1000,600+150*sin(t*2*PI+PI/3),372,158);
 		
 	}
 
@@ -90,6 +90,6 @@ function draw() {
 	ellipse(1000,120,4500,1500);
 
 	//젓가락
-	image(chopsticks,mouseX,mouseY,700,700);
+	image(chopsticks,mouseX,mouseY,500,500);
 
 }
