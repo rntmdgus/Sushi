cd555f791f3f0ae8f6a409e10f38c080b0d73c83
rntmdgus <rntmdgus@naver.com>
2014-11-20 14:27:34 +0900

    34534
    53566

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var bg, chopsticks, left, right, count1, count2, cat1, cat2, sushi1, sushi2, sushi3, dishw, dishy, dishr, dishb;
 
+var countCat;
 function setup() {
 
     createCanvas(2000, 1000);
@@ -20,6 +21,7 @@ function setup() {
 
 	count1 = 0;
 	count2 = 0;
+	countCat =0;
 }
 
 function draw() {
@@ -37,8 +39,8 @@ function draw() {
 
 	//발왼쪽 이동
 	image(left,count1,0);
-	count1=count1+20;
-	if(count1>-2000){
+	count1=count1-20;
+	if(count1<-2000){
 	count1=0;
 	} 
 
@@ -54,36 +56,44 @@ function draw() {
 	stroke(255);
 	ellipse(1000,1300,4000,1500);
 
- 	//위스시1-새우접시w
-	image(dishw,1000 + Math.cos(timing1*2*PI)*2000-250,1300 + Math.sin(timing1*2*PI)*750-100,500,200);
- 	//위스시1-새우
-	image(sushi1,1000 + Math.cos(timing1*2*PI)*2000-136,1300 + Math.sin(timing1*2*PI)*750-99,272,158);
 
- 	//위스시2-참치접시y
-	image(dishy,1000 + Math.cos(timing1*2*PI)*2000-250,1300 + Math.sin(timing1*2*PI)*750-100,500,200);
- 	//위스시2-참치
-	image(sushi2,1000 + Math.cos(timing1*2*PI)*2000-136,1300 + Math.sin(timing1*2*PI)*750-99,272,158);
-
-	//위스시3-소고기접시r
-	image(dishw,1000 + Math.cos(timing1*2*PI)*2000-250,1300 + Math.sin(timing1*2*PI)*750-100,500,200);
- 	//위스시3-소고기
-	image(sushi1,1000 + Math.cos(timing1*2*PI)*2000-136,1300 + Math.sin(timing1*2*PI)*750-99,272,158);
+	if(50*sin(t*2*PI)<0){
+		//위
+		image(sushi1,1000+cos(t*2*PI)*2000,200+50*sin(t*2*PI),30,30);
+	}
+	if(50*sin(t*2*PI+PI/3)<0){
+		image(sushi2,+cos(t*2*PI+PI/3)*2000,200+50*sin(t*2*PI+PI/3),30,30);
+		
+	}
 
 
+	if(countCat <50){
 	//고양이1
  	image(cat1,0,0,2000,1000);
-
+	} else if(countCat <100){
  	//고양이2
  	image(cat2,0,0,2000,1000);	
+	} else{
+	countCat=0;
+	}
+	countCat++;
+
+
+
+	if(50*sin(t*2*PI)>=0){
+		//위
+		image(sushi1,1000+cos(t*2*PI)*2000,200+50*sin(t*2*PI),40,40);
+	}
+	if(50*sin(t*2*PI+PI/3)>=0){
+		image(sushi2,+cos(t*2*PI+PI/3)*2000,200+50*sin(t*2*PI+PI/3),40,40);
+		
+	}
 
 
 	//아래라인
 	noFill();
 	stroke(255);
 	ellipse(1000,120,4500,1500);
-
-	//아래스시1
-	image(earth,1000 + Math.cos(timing1*2*PI)*2250-300,120 + Math.sin(timing1*2*PI)*750-300,600,600);
 
 	//젓가락
 	image(chopsticks,mouseX,mouseY,700,700);
